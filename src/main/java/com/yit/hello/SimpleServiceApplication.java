0196faf359171e96048b5a0653eb3dd9391a9024
Panumas <41285881+panumaspa@users.noreply.github.com>
2023-06-12 16:58:05 +0700

    test

--- src/main/java/com/yit/hello/SimpleServiceApplication.java
+++ src/main/java/com/yit/hello/SimpleServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimpleServiceApplication {
- //fsfdsfefs
+
  //fsfdsfsdf
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleServiceApplication.class, args);
