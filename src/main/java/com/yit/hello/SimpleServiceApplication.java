884264eb68c7a3f514fa67b0461bd10252546efd
superuoa <superuoa@gmail.com>
2023-06-16 12:18:14 +0700

    comit2

--- src/main/java/com/yit/hello/SimpleServiceApplication.java
+++ src/main/java/com/yit/hello/SimpleServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleServiceApplication {
  //fsfdsfefs
- //fsfdsfsdf 22222
+ //fsfdsfsdf 22222 3333
 // dskfjdskfds
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleServiceApplication.class, args);
